37b23304eeb7afe9ceffa201baab38d297fda54c
Pete VS <pete_vs88@me.com>
2021-02-24 19:28:39 -0500

    added home endpoint

--- api/server.js
+++ api/server.js
@@ -4,6 +4,11 @@ const accountsRouter = require('./accounts/accounts-router')
 const server = express();
 
 server.use(express.json());
+server.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome! Go to /accounts to get more info'
+    })
+})
 server.use('/accounts/', accountsRouter )
 
 module.exports = server;
